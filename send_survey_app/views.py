7e8b1b426c1e483222cd2e95521b059b6050175a
leongus1 <leongus1@gmail.com>
2020-10-05 12:31:57 -0700

    index and form setup

--- send_survey_app/views.py
+++ send_survey_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
 def index (request):
-    return HttpResponse("This route is working.")
+    return render(request, "index.html")
+
+def display(request):
+    if request=="POST":
+        context = request.POST
+    return render (request, "display.html", context)
+    
+        
+        
 
 # Create your views here.
